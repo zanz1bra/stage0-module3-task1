0b9d0fa85ccc77c4475656efc01e4f7af11b2da5
zanzibra <talbergaerika@gmail.com>
2022-10-05 19:36:56 +0300

    tasks done

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        System.out.println("This" + \n "is" +\n "my" +\n "first" +\n "Java" +\n "program");
+        System.out.println("This\n is\n my \n first \n Java \n program");
     }
 }
